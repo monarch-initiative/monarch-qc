0ddeb283cfd029637f794259afce2b955192440e
Corey Cox <69321580+amc-corey-cox@users.noreply.github.com>
2023-03-01 09:59:03 -0600

    Add and configure prettier

--- monarch-qc-dashboard/.eslintrc.cjs
+++ monarch-qc-dashboard/.eslintrc.cjs
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier'
+  ], 
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
