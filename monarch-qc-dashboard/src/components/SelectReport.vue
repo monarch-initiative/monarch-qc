9f65f3c2448e91601f30e01959befc85fdbea87a
Corey Cox <69321580+amc-corey-cox@users.noreply.github.com>
2023-04-14 17:32:16 -0500

    Add select for QC Report release

---
+++ monarch-qc-dashboard/src/components/SelectReport.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Select a report:</h1>
+    <select v-model="selectedReport" :style="{ padding: '0.5rem', borderRadius: '0.25rem' }">
+      <option v-for="[reportName, value] of reports" :key="reportName" :value="reportName">
+        {{ reportName }}
+      </option>
+    </select>
+    <div v-if="selectedReport">
+      <h2>{{ selectedReport }}</h2>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  // import { selectReport } from "./SelectReport"
+  import { watch } from "vue"
+  import { processReport } from "../data"
+  import { selectedReport } from "../data"
+
+  defineProps<{
+    reports: Map<string, Promise<string>>
+  }>()
+
+  watch(selectedReport, async () => {
+    await processReport()
+  })
+</script>
