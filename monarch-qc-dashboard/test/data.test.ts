69a7279e428b21e1a18f0e48e32e8746b026c9d1
Corey Cox <69321580+amc-corey-cox@users.noreply.github.com>
2023-11-09 08:30:10 -0600

    Finalize fetchQCReports testing

--- monarch-qc-dashboard/test/data.test.ts
+++ monarch-qc-dashboard/test/data.test.ts
@@ -25,16 +25,16 @@ const urls = [
   "https://data.monarchinitiative.org/monarch-kg-dev/2023-04-02/qc_report.yaml",
   "https://data.monarchinitiative.org/monarch-kg-dev/2023-10-28/index.html",
   "https://data.monarchinitiative.org/monarch-kg-dev/2023-10-28/qc_report.yaml",
+  "https://data.monarchinitiative.org/monarch-kg-dev/kgx/index.html",
+  "https://data.monarchinitiative.org/monarch-kg-dev/latest/index.html",
+  "https://data.monarchinitiative.org/monarch-kg-dev/latest/qc_report.yaml",
 ]
 
 const handlers = urls.map(createRequestHandler)
 const server = setupServer(...handlers)
 
-beforeAll(() => server.listen())
-afterAll(() => server.close())
-afterEach(() => server.resetHandlers())
-
 describe("fetchAllData tests", () => {
+  // @vitest-environment jsdom
   test("fetchAllData empty QCPart", () => {
     expect(true).toBe(true)
   })
@@ -42,13 +42,19 @@ describe("fetchAllData tests", () => {
 
 describe("fetchQCReports tests", () => {
   // @vitest-environment jsdom
-  test("fetchQCReports", async () => {
-    const url = "https://data.monarchinitiative.org/monarch-kg-dev/"
-    expect(url).toEqual("https://data.monarchinitiative.org/monarch-kg-dev/")
-    const response = await fetch(url)
-    const text = await response.text()
-    const QCReports = await fetchQCReports(text)
-    expect(QCReports).toEqual(
+  beforeAll(() => server.listen({ onUnhandledRequest: "error" }))
+  afterAll(() => server.close())
+  afterEach(() => server.resetHandlers())
+
+  test("fetchQCReports undefined", async () => {
+    const reports = await fetchQCReports(undefined)
+    expect(reports).toEqual(new Map())
+  })
+
+  test("fetchQCReports deep structure test", async () => {
+    const response = await fetch("https://data.monarchinitiative.org/monarch-kg-dev/")
+    const reports = await fetchQCReports(await response.text())
+    expect(reports).toEqual(
       new Map([
         [
           `2023-04-02`,
@@ -70,14 +76,14 @@ describe("fetchQCReports tests", () => {
         ],
       ])
     )
-    expect(await QCReports.get("2022-02-10")).toEqual(undefined)
-    expect(await QCReports.get("2023-04-02")).toEqual(
+    expect(await reports.get("2022-02-10")).toEqual(undefined)
+    expect(await reports.get("2023-04-02")).toEqual(
       readFileSync("test/mock_http/monarch-kg-dev/2023-04-02/qc_report.yaml", "utf-8")
     )
-    expect(await QCReports.get("2023-10-28")).toEqual(
+    expect(await reports.get("2023-10-28")).toEqual(
       readFileSync("test/mock_http/monarch-kg-dev/2023-10-28/qc_report.yaml", "utf-8")
     )
-    expect(await QCReports.get("latest")).toEqual(
+    expect(await reports.get("latest")).toEqual(
       readFileSync("test/mock_http/monarch-kg-dev/2023-10-28/qc_report.yaml", "utf-8")
     )
   })
